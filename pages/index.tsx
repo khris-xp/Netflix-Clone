d6f8eb4fa6bd802868832bf08d98be65595443f7
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-05 21:05:38 +0700

    add : create a banner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
@@ -11,11 +12,11 @@ const Home: NextPage = () => {
         <title>Netflix Clone</title>
         <link rel="icon" href="/favicon" />
       </Head>
-      
+
       <Header />
 
       <main>
-        {/* Banner */}
+        <Banner />
       </main>
     </div>
   )
