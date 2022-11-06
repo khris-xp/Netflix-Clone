07ceaa5ed2e236b127b2026b273e4fb0ea82bf44
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-11-06 22:46:53 +0700

    add : thumbnails component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,7 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('tailwindcss-textshadow')],
+  plugins: [
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar-hide')],
 }
